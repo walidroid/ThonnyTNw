ad5cce171022102cd99c9005f3bfdaed9bd8bf07
walidroid <38987907+walidroid@users.noreply.github.com>
2025-12-31 18:05:49 +0100

    Update __init__.py

--- thonny_quick_switch/__init__.py
+++ thonny_quick_switch/__init__.py
@@ -1,16 +1,9 @@
 from thonny import get_workbench
 
-def is_python3_active():
-    """Retourne True si l'interpréteur actuel est Python 3"""
-    return get_workbench().get_option("run.backend_name") == "LocalCPython"
-
-def is_esp32_active():
-    """Retourne True si l'interpréteur actuel est l'ESP32"""
-    # On vérifie si le nom contient ESP32
-    return get_workbench().get_option("run.backend_name") == "ESP32"
+def always_enabled():
+    return True
 
 def select_python3():
-    """Bascule vers Python 3"""
     wb = get_workbench()
     wb.set_option("run.backend_name", "LocalCPython")
     try:
@@ -19,7 +12,6 @@ def select_python3():
         pass
 
 def select_esp32():
-    """Bascule vers l'ESP32"""
     wb = get_workbench()
     wb.set_option("run.backend_name", "ESP32")
     try:
@@ -28,27 +20,22 @@ def select_esp32():
         pass
 
 def load_plugin():
-    """Initialise le plugin avec les options cochables dans le menu Outils"""
     wb = get_workbench()
-    
-    # Option Python 3
+
     wb.add_command(
         command_id="switch_to_python3",
         menu_name="tools",
         command_label="Mode : Python 3",
         handler=select_python3,
-        tester=is_python3_active, # Thonny ajoute le ✓ si cette fonction renvoie True
-        include_in_toolbar=False,
+        tester=always_enabled,
         group=110
     )
-    
-    # Option ESP32
+
     wb.add_command(
         command_id="switch_to_esp32",
         menu_name="tools",
         command_label="Mode : ESP32",
         handler=select_esp32,
-        tester=is_esp32_active,   # Thonny ajoute le ✓ si cette fonction renvoie True
-        include_in_toolbar=False,
+        tester=always_enabled,
         group=110
     )
